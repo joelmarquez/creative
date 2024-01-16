7ce3a0a08701bb83044784f6364fd34ddcbe2520
Joel <joel@magdalenamedio.net>
2024-01-16 15:53:58 -0500

    Se actualiza archivo sketch-skew.js y se instala libreria canvas-sketch-util

--- sketches/sketch-skew.js
+++ sketches/sketch-skew.js
@@ -1,40 +1,68 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [1080, 1080 ]
 };
 
 const sketch = () => {
   let x, y, w, h;
+  let radius, angle;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    x = width  * 0.5;
-    y = height * 0.5;
-    w = width  * 0.6;
-    h = height * 0.1;
+    x = width * 0.5;
+    y = width * 0.5;
+    w = width * 0.6;
+    h = width * 0.1;
 
     context.save();
-    // Se situa el rectangulo en el centro
-    context.translate(x,y);
+    context.translate(x, y);
+    context.translate(w * -0.5, h * -0.5);
 
-    context.strokeStyle = 'blue';
+    context.strokestyle = "blue";
+
+    // Dibujo directo
     // context.strokeRect(w * -0.5, h * -0.5, w, h);
 
-    // Dibujando el rectangulo punto por punto
+    // Dibujo punto a punto del rectangulo
+    // context.beginPath();
+    // context.moveTo(w * -0.5, h * -0.5);
+    // context.lineTo(w *  0.5, h * -0.5);
+    // context.lineTo(w *  0.5, h *  0.5);
+    // context.lineTo(w * -0.5, h *  0.5);
+    // context.closePath();
+    // context.stroke();
+
+    // Factorizando codigo de arriba
+    // context.beginPath();
+    // context.moveTo(0, 0);
+    // context.lineTo(w, 0);
+    // context.lineTo(w, h);
+    // context.lineTo(0, h);
+    // context.closePath();
+    // context.stroke();
+
+    radius = 200;
+    angle = math.degToRad(30);
+
+    x = Math.cos(angle) * radius;
+    y = Math.sin(angle) * radius;
+
+    console.log(x, y);
+
+    // Dibujando una linea
     context.beginPath();
-    context.moveTo(w * -0.6, h * -0.6);
-    context.lineTo(w * 0.6, h * -0.6);
-    context.lineTo(w * 0.6, h * 0.6);
-    context.lineTo(w * -0.6, h * 0.6);
-    context.closePath();
+    context.moveTo(0, 0);
+    context.lineTo(x, y);
     context.stroke();
 
     context.restore();
 
   };
 };
+
 
 canvasSketch(sketch, settings);
