4e6654289035cf0d3a74c11bf2200239faf66495
Joel <joel@magdalenamedio.net>
2024-01-17 12:15:58 -0500

    se realiza ejercicio de rectangulo sesgado

--- sketches/sketch-skew.js
+++ sketches/sketch-skew.js
@@ -7,7 +7,7 @@ const settings = {
 
 const sketch = () => {
   let x, y, w, h;
-  let radius, angle;
+  let angle, rx, ry;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -20,49 +20,58 @@ const sketch = () => {
 
     context.save();
     context.translate(x, y);
-    context.translate(w * -0.5, h * -0.5);
-
     context.strokestyle = "blue";
 
-    // Dibujo directo
-    // context.strokeRect(w * -0.5, h * -0.5, w, h);
+    drawSkewedRect({ context });
+    context.stroke();
+    
+    // radius = 200;
 
-    // Dibujo punto a punto del rectangulo
-    // context.beginPath();
-    // context.moveTo(w * -0.5, h * -0.5);
-    // context.lineTo(w *  0.5, h * -0.5);
-    // context.lineTo(w *  0.5, h *  0.5);
-    // context.lineTo(w * -0.5, h *  0.5);
-    // context.closePath();
-    // context.stroke();
 
-    // Factorizando codigo de arriba
-    // context.beginPath();
-    // context.moveTo(0, 0);
-    // context.lineTo(w, 0);
-    // context.lineTo(w, h);
-    // context.lineTo(0, h);
-    // context.closePath();
-    // context.stroke();
-
-    radius = 200;
-    angle = math.degToRad(30);
-
-    x = Math.cos(angle) * radius;
-    y = Math.sin(angle) * radius;
-
-    console.log(x, y);
+    // console.log(x, y);
 
     // Dibujando una linea
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(x, y);
-    context.stroke();
+    // context.beginPath();
+    // context.moveTo(0, 0);
+    // context.lineTo(x, y);
+    // context.stroke();
 
     context.restore();
 
   };
 };
 
+// Declaramos función
+const drawSkewedRect = ({ context, w = 300, h = 800, degrees = -45 }) => {
+  const angle = math.degToRad(degrees);
+  const rx = Math.cos(angle) * w;
+  const ry = Math.sin(angle) * w;
+
+  context.save();
+  context.translate(rx * -0.5,(ry + h) * -0.5);
+
+  // Dibujo directo
+  // context.strokeRect(w * -0.5, h * -0.5, w, h);
+
+  // Dibujo punto a punto del rectangulo
+  // context.beginPath();
+  // context.moveTo(w * -0.5, h * -0.5);
+  // context.lineTo(w *  0.5, h * -0.5);
+  // context.lineTo(w *  0.5, h *  0.5);
+  // context.lineTo(w * -0.5, h *  0.5);
+  // context.closePath();
+  // context.stroke();
+
+  // Factorizando codigo de arriba
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(rx, ry);
+  context.lineTo(rx, ry + h);
+  context.lineTo(0, h);
+  context.closePath();
+  context.stroke();
+
+  context.restore();
+}
 
 canvasSketch(sketch, settings);
